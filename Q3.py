54646a282d5010ca821da2e8148901095273354d
gursneh287 <144039255+gursneh287@users.noreply.github.com>
2023-11-28 10:37:38 +0530

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,7 +1,7 @@
 x = int(input("enter a number x:"))
 y = int(input("enter a number y:"))
 if x>=0 and y>=0:
-    if y != x == 0:
+    if y % x == 0:
         print("y is divisible by x")
     else:
         print("y is not divisible by x")
